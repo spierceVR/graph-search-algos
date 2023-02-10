d54c267367516989905555b356201f5689d8087d
Solomon Pierce <1nepthys1@gmail.com>
2023-02-10 01:31:28 -0600

    fix river crossing state and action validation

--- river_crossing.py
+++ river_crossing.py
@@ -1,114 +1,96 @@
 from heapq import heappop, heappush
 
-
-state: list[int] = [3, 3, 0]
-actions: list[str] = ['1,0', '2,0', '0,1', '0,2', '1,1']
-
-
-def goal_test(state: list[int]):
-    return state == [0, 0, 1]
+# cats, mice, boat
+state: tuple[int] = (3, 3, 0)
 
 
-def is_valid(state: list[int]):
-    if state[0] < 0 or state[1] < 0 or state[0] > 3 or state[1] > 3:
-        return False
-    elif state[0] > 0 and state[0] < state[1]:
-        return False
-    elif state[0] < 3 and state[0] > state[1]:
-        return False
-    else:
-        return True
+def goal_test(state: tuple[int, int, int]):
+    return state == (0, 0, 1)
 
 
-def heuristic(state: list[int]):
+def heuristic(state: tuple[int]):
     return state[0] + state[1]
 
 
-def uniform_cost_search(state: list[int], actions: list[str]):
+def valid_actions(state) -> list[str]:
+    actions: list[str] = ['1,0', '2,0', '0,1', '0,2', '1,1']
+    valid_actions = []
+    if state[2] == 0:
+        for action in actions:
+            # if there are enough cats and mice on the left side to take this action
+            if int(action[0]) <= state[0] and int(action[2]) <= state[1]:
+                valid_actions.append(action)
+    else:
+        for action in actions:
+            # if there are enough cats and mice on the right side to take this action
+            if int(action[0]) <= 3 - state[0] and int(action[2]) <= 3 - state[1]:
+                valid_actions.append(action)
+    return valid_actions
+
+
+def valid_states(state) -> list[tuple[int]]:
+    valid_states = []
+    actions = valid_actions(state)
+    if actions == None:
+        return []
+    for action in actions:
+        if state[2] == 0:
+            valid_states.append(
+                (state[0] - int(action[0]), state[1] - int(action[2]), 1))
+        else:
+            valid_states.append(
+                (state[0] + int(action[0]), state[1] + int(action[2]), 0))
+    return [x for x in valid_states if valid_combination(x)]
+
+
+def valid_combination(state) -> bool:
+    if state[0] > 0 and state[1] > 0:
+        if state[0] > state[1]:
+            return False  # mice outnumbered by cats on the left side
+    cats_right = 3 - state[0]
+    mice_right = 3 - state[1]
+    if cats_right > 0 and mice_right > 0:
+        if cats_right > mice_right:
+            return False  # mice outnumbered by cats on the right side
+    if state[0] < 0 or state[1] < 0:
+        return False  # negative number of cats or mice
+    if state[0] > 3 or state[1] > 3:
+        return False  # more cats or mice than allowed
+    return True
+
+
+def uniform_cost_search(state: tuple[int]) -> tuple[list[tuple[int]], int] or None:
     queue = [(0, state, [])]
     nodes_expanded = 0
+    seen = set()
     while queue:
         (cost, node, path) = heappop(queue)
         nodes_expanded += 1
         print("expanding node: " + str(node) + "\twith cost: " + str(cost))
         if goal_test(node):
+            print(f"node {node} is the goal")
             return (path + [node], nodes_expanded)
-        for action in actions:
-            action = action.split(',')
-            action = [int(action[0]), int(action[1])]
-            new_state = [node[0] - action[0], node[1] - action[1], node[2]]
-            if node[2] == 0:
-                new_state[0] += action[0]
-                new_state[1] += action[1]
-                new_state[2] = 1
-            else:
-                new_state[2] = 0
-            if is_valid(new_state):
-                g = cost + 1
-                heappush(queue, (g, new_state, path + [node]))
-                print(f"{new_state} with: g({new_state}) = {g}")
+        successors = valid_states(node)
+        print("successors are:")
+        for new_state in successors:
+            if new_state in seen:
+                continue
+            seen.add(new_state)
+            g = cost + 1
+            heappush(queue, (g, new_state, path + [node]))
+            print(f"{new_state} with: g({new_state}) = {g}")
         print()
+    return (None, nodes_expanded)
 
 
-def greedy_best_first_search(state: list[int], actions: list[str]):
-    queue = [(0, state, [])]
-    nodes_expanded = 0
-    while queue:
-        (cost, node, path) = heappop(queue)
-        nodes_expanded += 1
-        print("expanding node: " + str(node) +
-              "\twith heuristic: " + str(cost))
-        if goal_test(node):
-            return (path + [node], nodes_expanded)
-        for action in actions:
-            action = action.split(',')
-            action = [int(action[0]), int(action[1])]
-            new_state = [node[0] - action[0], node[1] - action[1], node[2]]
-            if node[2] == 0:
-                new_state[0] += action[0]
-                new_state[1] += action[1]
-                new_state[2] = 1
-            else:
-                new_state[2] = 0
-            if is_valid(new_state):
-                h = heuristic(new_state)
-                heappush(queue, (h, new_state, path + [node]))
-                print(f"{new_state} with: h({new_state}) = {h}")
-        print()
 
-
-def depth_first_search(state: list[int], actions: list[str]):
-    stack = [(state, [])]
-    nodes_expanded = 0
-    while stack:
-        (node, path) = stack.pop()
-        nodes_expanded += 1
-        print("expanding node: " + str(node))
-        if goal_test(node):
-            return (path + [node], nodes_expanded)
-        for action in actions:
-            action = action.split(',')
-            action = [int(action[0]), int(action[1])]
-            new_state = [node[0] - action[0], node[1] - action[1], node[2]]
-            if node[2] == 0:
-                new_state[0] += action[0]
-                new_state[1] += action[1]
-                new_state[2] = 1
-            else:
-                new_state[2] = 0
-            if is_valid(new_state):
-                stack.append((new_state, path + [node]))
-                print(f"{new_state} added to stack")
-        print()
-
-
-(UCS_path, nodes_expanded) = uniform_cost_search(state, actions)
-(greedy_path, greedy_nodes_expanded) = greedy_best_first_search(state, actions)
-(depth_path, depth_nodes_expanded) = depth_first_search(state, actions)
+(UCS_path, nodes_expanded) = uniform_cost_search(state)
+# (greedy_path, greedy_nodes_expanded) = greedy_best_first_search(state, actions)
+# (depth_path, depth_nodes_expanded) = depth_first_search(state, actions)
 
 print(UCS_path)
 print(nodes_expanded)
-print(greedy_path)
-print(greedy_nodes_expanded)
-print(depth_path)
-print(depth_nodes_expanded)
+# print(greedy_path)
+# print(greedy_nodes_expanded)
+# print(depth_path)
+# print(depth_nodes_expanded)
